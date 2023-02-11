a2f28eadff56588798db232d38ab6081021aa07f
Jerry <jamesharold1738@gmail.com>
2023-02-11 18:29:53 -0500

    Added map.js

--- assets/script.js
+++ assets/script.js
@@ -26,7 +26,6 @@ video.setAttribute("src", youtubeLink );
 
  const randomColor = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
-  color.innerHTML = "#" + randomColor;
 
 
 
